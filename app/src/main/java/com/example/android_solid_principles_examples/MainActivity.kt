0f64c98685b736ea32c5de99a6d24f2e329283fc
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-09 13:45:14 -0500

    Update

--- app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
@@ -13,28 +13,35 @@ class MainActivity : AppCompatActivity() {
 
         tvWelcome.text = "Welcome to my application!"
 
-        //Ask for user information
-        val person = Person()
-
         tvFirstName.text = "What is your first name?"
-        person.firstName = etFirstName.text.toString()
 
         tvLastName.text = "What is your last name?"
-        person.lastName = etLastName.text.toString()
 
-        //Checks to be sure the first name and last names are valid
-        if(person.firstName.isNullOrEmpty()){
-            Toast.makeText(this, "You didn't give us a valid first name!", Toast.LENGTH_SHORT).show()
-            return
+        btnCreateAccountName.setOnClickListener {
+            //Ask for user information
+            val person = Person()
+
+            person.firstName = etFirstName.text.toString()
+
+            person.lastName = etLastName.text.toString()
+
+            //Checks to be sure the first name and last names are valid
+            if (person.firstName.isNullOrEmpty()) {
+                Toast.makeText(this, "You didn't give us a valid first name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (person.lastName.isNullOrEmpty()) {
+                Toast.makeText(this, "You didn't give us a valid last name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //Create a user name for the person
+            Toast.makeText(
+                this,
+                "YOUR USERNAME IS ${person.firstName!!.substring(0, 1)} ${person.lastName}",
+                Toast.LENGTH_LONG
+            ).show()
         }
-
-        if(person.lastName.isNullOrEmpty()){
-            Toast.makeText(this, "You didn't give us a valid last name!", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        //Create a user name for the person
-        Toast.makeText(this, "YOUR USERNAME IS ${person.firstName!!.substring(0,1)} ${person.lastName}", Toast.LENGTH_LONG).show()
-
     }
 }
