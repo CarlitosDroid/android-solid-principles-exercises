a3653dfa0fee11b54e044b2234603ea4618943c0
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-09 18:00:09 -0500

    Update

--- app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
@@ -11,30 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvWelcome.text = "Welcome to my application!"
-
-        //Ask for user information
-        val person = Person()
-
-        tvFirstName.text = "What is your first name?"
-        person.firstName = etFirstName.text.toString()
-
-        tvLastName.text = "What is your last name?"
-        person.lastName = etLastName.text.toString()
-
-        //Checks to be sure the first name and last names are valid
-        if(person.firstName.isNullOrEmpty()){
-            Toast.makeText(this, "You didn't give us a valid first name!", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if(person.lastName.isNullOrEmpty()){
-            Toast.makeText(this, "You didn't give us a valid last name!", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        //Create a user name for the person
-        Toast.makeText(this, "YOUR USERNAME IS ${person.firstName!!.substring(0,1)} ${person.lastName}", Toast.LENGTH_LONG).show()
-
     }
 }
--- app/src/main/java/com/example/android_solid_principles_examples/Person.kt
+++
@@ -1,3 +0,0 @@
-package com.example.android_solid_principles_examples
-
-class Person(var firstName: String? = null, var lastName: String? = null)
