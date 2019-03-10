f5ba247110935fad236b95110ddae5c560f09b3d
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-09 23:47:58 -0500

    Update srp exercises 1

--- app/src/main/java/com/example/android_solid_principles_examples/CustomTextView.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/CustomTextView.kt
@@ -1,16 +1,50 @@
 package com.example.android_solid_principles_examples
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.AsyncTask
 import android.util.AttributeSet
 import android.widget.TextView
+import android.widget.Toast
 
 class CustomTextView : TextView {
+
+    var user = User()
 
     constructor (context: Context) : super(context)
 
     constructor (context: Context, attrs: AttributeSet?) : super(context, attrs)
 
-    fun render(user: User) {
-        text = context.getString(R.string.user_information_message, user.name, user.lastName, user.age)
+    fun getUserInfoById(userId: Int) {
+        UserInfoAsyncTask(userId).execute()
+    }
+
+    private fun render() {
+        text = "Name: ${user.name} \nLastName: ${user.lastName} \nAge: ${user.age}"
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class UserInfoAsyncTask(var userId: Int) : AsyncTask<Void, Void, User>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Toast.makeText(context, "Requesting User Info with id ${userId}…", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun doInBackground(vararg params: Void?): User {
+            val user = User()
+            Thread.sleep(3000)
+            user.id = userId
+            user.name = "Carlos"
+            user.lastName = "Vargas"
+            user.age = 20
+            return user
+        }
+
+        override fun onPostExecute(user: User) {
+            super.onPostExecute(user)
+            this@CustomTextView.user = user
+            render()
+        }
     }
 }
--- app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.android_solid_principles_examples
 
-import android.annotation.SuppressLint
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,29 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         btnRequestUserInfo.setOnClickListener {
             val userId = 1
-            UserInfoAsyncTask(userId).execute()
-        }
-    }
-
-    private fun showUserMessage(string: String) {
-        Toast.makeText(this, string, Toast.LENGTH_LONG).show()
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    inner class UserInfoAsyncTask(var userId: Int) : AsyncTask<Void, Void, User>() {
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            showUserMessage(getString(R.string.request_user_message, userId))
-        }
-
-        override fun doInBackground(vararg params: Void?): User {
-            return UserManager.getUserById(userId)
-        }
-
-        override fun onPostExecute(user: User) {
-            super.onPostExecute(user)
-            ctvUserInfo.render(user)
+            ctvUserInfo.getUserInfoById(userId)
         }
     }
 }
