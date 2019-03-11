336bbfa8a3793d8f4f35394c0caa66b207161613
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-10 19:18:13 -0500

    Update

--- app/src/main/java/com/example/android_solid_principles_examples/SalaryCalculator.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/SalaryCalculator.kt
@@ -14,7 +14,7 @@ class SalaryCalculator(private var developerReportList: List<DeveloperReport>) {
      * But what happens if our boss wants a different calculation for the senior and junior developer
      * The senior developer should have a bonus of 20% on a salary.
      */
-    /*fun calculateTotalSalary1(): Double {
+    /*fun calculateTotalSalary(): Double {
         var totalSalaries = 0.0
         for (developerReport in developerReportList) {
             totalSalaries += if (developerReport.level == "Senior developer")
