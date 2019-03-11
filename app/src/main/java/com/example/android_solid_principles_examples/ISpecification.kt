281e6365562c93683e7f29c0b989b30345ea3b0b
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-11 00:56:42 -0500

    Update

--- app/src/main/java/com/example/android_solid_principles_examples/ISpecification.kt
+++ app/src/main/java/com/example/android_solid_principles_examples/ISpecification.kt
@@ -1,5 +1,8 @@
 package com.example.android_solid_principles_examples
 
+/**
+ * We can determine whether or not our criterion is satisfied
+ */
 interface ISpecification<T> {
     fun isSatisfied(item: T): Boolean
 }
